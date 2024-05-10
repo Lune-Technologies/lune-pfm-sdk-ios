00d2e9609bf2ce111b689f24ce624b5f7dbece27
Zamorite <zamorite@gmail.com>
2024-05-10 09:44:47 +0400

    chore: 🎯 add target an update package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LuneSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LuneSDK",
+            targets: ["LuneSDK"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "LuneSDK",
+            url: "./LuneSDK.xcframework.zip",
+            checksum: "5b12ac01ab2b276a59962dd9a0dbce9cac81be83cab4921e7fc6a76362fa253a"
+        )
+    ]
+)
