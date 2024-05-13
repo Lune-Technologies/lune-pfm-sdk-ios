caf26c55757fd66c2da50dbe64172fd8b35f9d9d
Zamorite <zamorite@gmail.com>
2024-05-13 17:26:37 +0400

    chore: 📦 add xcframework directly

---
+++ LuneSDK.xcframework/ios-arm64/LuneSDK.framework/Headers/LuneSDK.h
@@ -0,0 +1,19 @@
+//
+//  LuneKit.h
+//  LuneKit
+//
+//  Created by Zamorite on 25/10/2022.
+//
+
+#import <UIKit/UIKit.h>
+#import <SwiftUI/SwiftUI.h>
+// #import <Foundation/Foundation.h>
+
+//! Project version number for LuneKit.
+FOUNDATION_EXPORT double LuneKitVersionNumber;
+
+//! Project version string for LuneKit.
+FOUNDATION_EXPORT const unsigned char LuneKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LuneKit/PublicHeader.h>
+
